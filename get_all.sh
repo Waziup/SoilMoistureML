5ed12136590175cb51e27b60cc85c7d91f139e97
Corentin Dupont <corentin.dupont@gmail.com>
2018-12-05 14:24:17 +0100

    scripts

--- get_all.sh
+++ get_all.sh
@@ -1,13 +1,18 @@
 
-SENSORS=`curl -X GET "http://api.waziup.io/api/v1/sensors?limit=1000" | jq -r ".[].id"`
+SENSORS=`curl --silent -X GET "http://api.waziup.io/api/v1/sensors?limit=1000" | jq -r ".[].id" 2>/dev/null`
+
+N=`echo "$SENSORS" | wc -l`
+
+echo "********** Getting data from $N sensors *********"
+COUNTER=1
 
 for i in $SENSORS; do
-
-  MEAS=`curl -X GET "http://api.waziup.io/api/v1/sensors/$i/measurements" | jq -r ".[].id"`
   
+  MEAS=`curl --silent -X GET "http://api.waziup.io/api/v1/sensors/$i/measurements" | jq -r ".[].id" 2>/dev/null`
+  COUNTER=$[$COUNTER +1]
   for j in $MEAS; do
-    echo "\n\nGetting data for sensor $i measurement $j"
-    ./get_data.sh $i $j
+    echo "$COUNTER: Getting data for sensor $i measurement $j"
+    ./get_data.sh $i $j 
   done
 
 done
--- get_data.sh
+++ get_data.sh
@@ -1,9 +1,28 @@
 
 FILENAME=$1-$2
 echo "" > plots/$FILENAME.csv
+
+# Accumulate datapoints
 for i in {0..10}; do
-  curl -X GET "http://api.waziup.io/api/v1/sensors/$1/measurements/$2/values?limit=1000&offset=$(($i * 1000))" -H  "accept: application/json" | jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' >> plots/$FILENAME.csv 
+  RES=`curl --silent -X GET "http://api.waziup.io/api/v1/sensors/$1/measurements/$2/values?limit=1000&offset=$(($i * 1000))" -H  "accept: application/json"`
+  
+  if [ "$RES" == "[]" ] 
+  then
+     break
+  fi
+
+  echo "$RES" | jq -r '(map(keys) | add | unique) as $cols | map(. as $row | $cols | map($row[.])) as $rows | $cols, $rows[] | @csv' 2>/dev/null >> plots/$FILENAME.csv 
+  
+  echo -n "."
 done
-
-gnuplot -e "filename='plots/$FILENAME'" plot 
-
+COUNT=`cat plots/$FILENAME.csv | wc -l`
+if [ $COUNT -gt 100 ]
+then
+   echo "Plotting plots/$FILENAME.png"
+   #plot the graph
+   gnuplot -e "filename='plots/$FILENAME'" plot 
+else
+   echo "Not enough data"
+   rm plots/$FILENAME.csv
+fi
+echo
